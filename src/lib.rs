dcc6e095c5b14cbace1c7803c9ded83b9866f9c5
nathanlaan <722624+NathanLaan@users.noreply.github.com>
2025-04-18 09:01:27 -0600

    docs: Updated code documentation

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,10 @@ pub const FONT_FA_REGULAR: Font = Font {
     ..Font::DEFAULT
 };
 
+//
+// Font data
+//
+
 const FONT_DATA_FA_REGULAR: &[u8] =
     include_bytes!("../fonts/font-awesome-6-free-regular-400.otf");
 
@@ -48,40 +52,59 @@ const FONT_DATA_FA_BRANDS: &[u8] =
 const FONT_DATA_FA_SOLID: &[u8] =
     include_bytes!("../fonts/font-awesome-6-free-solid-900.otf");
 
-///
+//
+// File operations
+//
+
 /// Font Awesome Unicode string for `https://fontawesome.com/icons/user`.
-///
 pub const FA_ICON_USER: &str = "f007";
 
-///
 /// Font Awesome Unicode string for `https://fontawesome.com/icons/file`.
-///
 pub const FA_ICON_NEW: &str = "f15b";
 
-///
 /// Font Awesome Unicode string for `https://fontawesome.com/icons/folder-open`.
-///
 pub const FA_ICON_OPEN: &str = "f07c";
 
-///
 /// Font Awesome Unicode string for `https://fontawesome.com/icons/floppy-disk`.
-///
 pub const FA_ICON_SAVE: &str = "f0c7";
 
-///
-/// Font Awesome Unicode string for `https://fontawesome.com/icons/1`.
-///
+//
+// Numbers
+//
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/0`.
 pub const FA_ICON_0: &str = "30";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/1`.
 pub const FA_ICON_1: &str = "31";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/2`.
 pub const FA_ICON_2: &str = "32";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/3`.
 pub const FA_ICON_3: &str = "33";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/4`.
 pub const FA_ICON_4: &str = "34";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/5`.
 pub const FA_ICON_5: &str = "35";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/6`.
 pub const FA_ICON_6: &str = "36";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/7`.
 pub const FA_ICON_7: &str = "37";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/8`.
 pub const FA_ICON_8: &str = "38";
+
+/// Font Awesome Unicode string for `https://fontawesome.com/icons/9`.
 pub const FA_ICON_9: &str = "39";
 
+//
+// Circle
+//
 
 /// Font Awesome Unicode string for https://fontawesome.com/icons/circle-check
 pub const FA_ICON_CIRCLE_CHECK: &str = "f058";
